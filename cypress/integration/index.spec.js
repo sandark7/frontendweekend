0609176ad5394deb0e4431d73953c83c1caf963b
Artem Tsatsin <nuxdie@gmail.com>
2018-12-07 23:28:00 +0100

    fix #21 (#26)
    ![127 0 0 1_8080_ 1](https://user-images.githubusercontent.com/3918844/49675517-58574780-fa76-11e8-9f98-f0f6129ae1a7.png)

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -106,4 +106,21 @@ describe('index page', () => {
         })
     })
 
+    describe('mobile version', () => {
+
+        it('should have all widgets at 100% width', () => {
+            cy.viewport('iphone-5')
+            cy.get('iframe.test--sc-embedded-player')
+                .should('have.css', 'width')
+                .and('lessThan', '321px')
+            cy.get('iframe.test--yandex-money-embed')
+                .should('have.css', 'width')
+                .and('lessThan', '321px')
+            cy.get('footer.test--footer')
+                .should('have.css', 'width')
+                .and('lessThan', '321px')
+        })
+
+    })
+    
 })
