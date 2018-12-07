decb473ce63e4af9913bb4cc293c7841fd4852a3
Artem Tsatsin <nuxdie@gmail.com>
2018-12-07 23:17:16 +0100

    fix #21

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -106,4 +106,20 @@ describe('index page', () => {
         })
     })
 
+    describe('mobile version', () => {
+
+        it('should have all widgets at 100% width', () => {
+            cy.viewport('iphone-5')
+            cy.get('iframe.test--sc-embedded-player')
+                .should('have.css', 'width')
+                .and('equal', '304px')
+            cy.get('iframe.test--yandex-money-embed')
+                .should('have.css', 'width')
+                .and('equal', '304px')
+            cy.get('footer.test--footer')
+                .should('have.css', 'width')
+                .and('equal', '304px')
+        })
+
+    })
 })
