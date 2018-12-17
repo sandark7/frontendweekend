3b3eb2ddc293862d03fdde0850d6c1b256e73e79
Artem Tsatsin <nuxdie@gmail.com>
2018-12-17 21:46:11 +0100

    Feat/sitemap (#77)

--- cypress/integration/index/metadata.js
+++ cypress/integration/index/metadata.js
@@ -18,4 +18,10 @@ describe('metadata', () => {
     cy.get('head meta[name="description"]')
       .should('have.attr', 'content', 'Самые честные интервью с известными людьми из мира web-разработки. Впечатляющие истории успеха, забавные моменты из жизни и полезные советы – мы показываем человеческое лицо frontend’а и не только.')
   })
+
+  it('should have a sitemap', () => {
+    cy.request('http://127.0.0.1:8080/sitemap.xml')
+      .its('body')
+      .should('contain', 'schemas/sitemap')
+  })
 })
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,50 @@
 module.exports = {
-    plugins: [
-        {
-            resolve: `gatsby-plugin-google-tagmanager`,
-            options: {
-                id: "GTM-T93788N",
-                // Include GTM in development.
-                includeInDevelopment: true,
-            },
-        },
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography.js`,
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-html-attributes',
-            options: {
-                lang: 'en'
-            }
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `content`,
-                path: `${__dirname}/content/`,
-            },
-        },
-        `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Frontend Weekend podcast`,
-                short_name: `FW`,
-                start_url: `/`,
-                background_color: `#ffee4e`,
-                theme_color: `#333333`,
-                display: `standalone`,
-                icon: `static/img/android-chrome-384x384.png`, // This path is relative to the root of the site.
-            },
-        },
-        `gatsby-plugin-offline`
-    ]
+  siteMetadata: {
+    siteUrl: `https://frontendweekend.ml`,
+  },
+  plugins: [
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: 'GTM-T93788N',
+        // Include GTM in development.
+        includeInDevelopment: true,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en'
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Frontend Weekend podcast`,
+        short_name: `FW`,
+        start_url: `/`,
+        background_color: `#ffee4e`,
+        theme_color: `#333333`,
+        display: `standalone`,
+        icon: `static/img/android-chrome-384x384.png`,
+      },
+    },
+    `gatsby-plugin-offline`
+  ]
 };
