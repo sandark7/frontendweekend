cabbbb94561dcc88fe0074e22c5b024ba410e9b9
Artem Tsatsin <nuxdie@gmail.com>
2018-12-24 19:22:45 +0100

    add import-sc-and-publish-daily workflow (#94)
    * add import-sc-and-publish-daily workflow
    
    * adjust commit to master hook
    
    * adjust commit to master hook

---
+++ commit-to-master.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git config credential.helper 'cache --timeout=120'
+git config user.email "sc-import-bot@frontendweekend.ml"
+git config user.name "SC Import Bot"
+git add .
+git commit -am "new episodes commit"
+git push -q https://${SC_IMPORT_GITHUB_TOKEN}@github.com/nuxdie/frontendweekend.git master
