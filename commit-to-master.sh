19014cf24636ad50b72398fd2a4957ae7228f645
Artem Tsatsin <artem.tsatsin@booking.com>
2018-12-28 12:30:57 +0100

    tidy up a bit

--- commit-to-master.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git config credential.helper 'cache --timeout=120'
-git config user.email "sc-import-bot@frontendweekend.ml"
-git config user.name "SC Import Bot"
-git add .
-git commit -am "new episodes commit"
-git push -q https://${SC_IMPORT_GITHUB_TOKEN}@github.com/nuxdie/frontendweekend.git master
