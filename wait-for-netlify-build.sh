287ad8649faadbcf4c973a9a1130736c2ab5d6f1
Artem Tsatsin <artem.tsatsin@booking.com>
2018-12-30 21:15:43 +0100

    fix curl request

--- wait-for-netlify-build.sh
+++ wait-for-netlify-build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
-    SITE_URL = "https://frontendweekend.ml"
+    SITE_URL="https://frontendweekend.ml"
 else
     PR_NUMBER=`echo ${CIRCLE_PULL_REQUEST##*/}`
-    SITE_URL = "https://deploy-preview-${PR_NUMBER}--frontendweekend.netlify.com"
+    SITE_URL="https://deploy-preview-${PR_NUMBER}--frontendweekend.netlify.com"
 fi
 
-echo "Waiting for ${SITE_URL} to come alive"
-until $(curl --output /dev/null --silent --head --fail ); do
+echo "Waiting for $SITE_URL to come alive"
+until $(curl --output /dev/null --silent --head --fail $SITE_URL); do
     printf '.'
     sleep 5
 done
