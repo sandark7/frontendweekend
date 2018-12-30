ee445c27da9caa3714bd3d0749c30cd2ff3f1dd6
Artem Tsatsin <artem.tsatsin@booking.com>
2018-12-30 20:54:02 +0100

    fix master lh test

--- lighthouse.test.js
+++ lighthouse.test.js
@@ -1,10 +1,17 @@
 const lighthouse = require("lighthouse"); // Node CLI for Lighthouse https://www.npmjs.com/package/lighthouse#using-the-node-cli
 const chromeLauncher = require("chrome-launcher"); // Launch Chrome from node
 
-jest.setTimeout(60000);
+jest.setTimeout(60000)
 
-const PR_NUMBER = (process.env['CIRCLE_PULL_REQUEST']).match(/(\d+)$/)[1]
-const STAGE_URL = `https://deploy-preview-${PR_NUMBER}--frontendweekend.netlify.com`
+let SITE_URL
+
+var PRODUCTION_BUILD = process.env['CIRCLE_BRANCH'] === 'master'
+if (PRODUCTION_BUILD) {
+  SITE_URL = `https://frontendweekend.ml`
+} else {
+  const PR_NUMBER = (process.env['CIRCLE_PULL_REQUEST']).match(/(\d+)$/)[1]
+  SITE_URL = `https://deploy-preview-${ PR_NUMBER }--frontendweekend.netlify.com`
+}
 
 const launchChromeAndRunLighthouse = (
     url,
@@ -19,12 +26,12 @@ const launchChromeAndRunLighthouse = (
     });
 
 test("All categories score", () =>
-    launchChromeAndRunLighthouse(STAGE_URL).then(
+    launchChromeAndRunLighthouse(SITE_URL).then(
         ({lhr: {categories}}) => {
             expect(categories["accessibility"].score).toBeGreaterThanOrEqual(1);
             expect(categories["best-practices"].score).toBeGreaterThanOrEqual(1);
             expect(categories["performance"].score).toBeGreaterThanOrEqual(0.9);
             expect(categories["pwa"].score).toBeGreaterThanOrEqual(1);
-            expect(categories["seo"].score).toBeGreaterThanOrEqual(0.8);
+            expect(categories["seo"].score).toBeGreaterThanOrEqual(PRODUCTION_BUILD ? 1 : 0.8);
         }
     ));
