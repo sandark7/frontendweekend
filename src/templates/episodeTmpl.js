3d52239514a15b01abbd3bbe0e27a2b8de87325a
Artem Tsatsin <nuxdie@gmail.com>
2018-12-31 00:42:41 +0100

    add 2 more random episodes to index and episode pages (#125)
    add 2 more random episodes to index and episode pages  …
    this closes #62
    
    +
    
    refactor support us component

--- src/templates/episodeTmpl.js
+++ src/templates/episodeTmpl.js
@@ -3,19 +3,31 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Episode from '../components/episode'
 import { I18n } from 'react-i18next'
-import { withI18next } from 'gatsby-plugin-i18next'
+import { Link, withI18next } from 'gatsby-plugin-i18next'
+import RandomEpisodes from '../components/randomEpisodes'
+import uniqueRandomArray from 'unique-random-array'
+import LayoutCSSModule from '../components/layout.module.css'
+import Support from '../components/support'
+import EpisodeTmplCSSModule from './episodeTmpl.module.css'
 
 class EpisodeTmpl extends Component {
   render () {
-    const {
+    let {
       lng,
       data: {
-        markdownRemark: episode,
+        currentEpisode: episode,
         site: {
           siteMetadata: { siteUrl },
-        }
+        },
+        allEpisodes: { edges: allEpisodes }
       }
     } = this.props
+    allEpisodes = allEpisodes.filter(({ node }) => (node.id !== episode.id))
+    const getRandomEpisode = uniqueRandomArray(allEpisodes)
+    const randomEpisodes = [
+      getRandomEpisode(),
+      getRandomEpisode()
+    ]
     return (
       <I18n>
         {t => (
@@ -26,12 +38,47 @@ class EpisodeTmpl extends Component {
           ].join(' ')}
           description={episode.frontmatter.subtitle}
           >
-            <Episode
-              episode={episode}
-              t={t}
-              lng={'/' + lng}
-              siteUrl={siteUrl}
-            />
+            <div
+              className={[
+                EpisodeTmplCSSModule.wrapper,
+                'test--wrapper',
+              ].join(' ')}
+            >
+              <div className={[
+                EpisodeTmplCSSModule.header_nav,
+                'test--header_nav',
+              ].join(' ')}>
+                <Link className={[
+                  EpisodeTmplCSSModule.archive_link,
+                  'test--header_nav-archive',
+                ].join(' ')} to={`/archive/#${ episode.frontmatter.name }`}>
+                  <span>{t('archive_link_text_back')}</span>
+                </Link>
+                <Link className={[
+                  LayoutCSSModule.nav_item,
+                  LayoutCSSModule.nav_item_subscribe,
+                  'test--header_nav-subscribe',
+                  'gtm--subscribe_link',
+                ].join(' ')} to={`/subscribe/`}>
+                  <span>{t('subscribe_podcast_link_text')}</span>
+                </Link>
+              </div>
+              <Episode
+                episode={episode}
+                t={t}
+                lng={'/' + lng}
+                siteUrl={siteUrl}
+              />
+              <RandomEpisodes
+                t={t}
+                randomEpisodes={randomEpisodes}
+              />
+              <Support
+                t={t}
+                siteUrl={siteUrl}
+                lng={lng}
+              />
+            </div>
           </Layout>
         )}
       </I18n>
@@ -53,15 +100,31 @@ export const query = graphql`
                 siteUrl
             }
         }
-        markdownRemark(fields: { slug: { eq: $slug } }) {
+        currentEpisode: markdownRemark(fields: { slug: { eq: $slug } }) {
             htmlAst
             frontmatter {
                 title
+                name
                 podcastUrl
                 subtitle
             }
             fields {
                 slug
+            }
+        }
+        allEpisodes: allMarkdownRemark{
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        subtitle
+                        image
+                    }
+                    fields {
+                        slug
+                    }
+                }
             }
         }
     }
