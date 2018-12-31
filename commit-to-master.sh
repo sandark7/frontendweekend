8893645a646d948393c0e64b0288ba4854ee5744
Artem Tsatsin <artem.tsatsin@booking.com>
2018-12-31 21:16:14 +0100

    fix commit sc-import message

--- commit-to-master.sh
+++ commit-to-master.sh
@@ -4,5 +4,5 @@ git config credential.helper 'cache --timeout=120'
 git config user.email "sc-import-bot@frontendweekend.ml"
 git config user.name "SC Import Bot"
 git add .
-git commit -am "new episodes commit"
+git commit -am "new episodes & comments commit"
 git push -q https://${SC_IMPORT_GITHUB_TOKEN}@github.com/nuxdie/frontendweekend.git master
