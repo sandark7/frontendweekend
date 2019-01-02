e9f1a38cb982864aa3fe1184ba9fbbd623d3ee94
nuxdie <nuxdie@gmail.com>
2019-01-02 22:19:04 +0100

    refactor sc-file-import

--- sc-file-import.js
+++ sc-file-import.js
@@ -6,39 +6,44 @@ const { readEpisodeFile } = require('./import/helpers')
 const EPISODE_MD_DIR = path.join(__dirname, './content/episode')
 const ASSET_FILE_DIR = path.join(__dirname, './static/assets')
 
+const noPodcastFile = ({ file, contents }) => {
+  let podcastFile
+  try {
+    podcastFile = contents.match(/podcastFile: (.+)/)[1]
+  } catch (e) {
+    return true
+  }
+  return !podcastFile || !podcastFile.length
+}
+
+const stringifyContent = f => {
+  f.contents += ''
+  return f
+}
+
+const downloadAndSave = ({ file, contents }) => {
+  const podcastUrl = contents.match(/podcastUrl: '(.+)'/)[1]
+  try {
+    downloadFile(podcastUrl, ASSET_FILE_DIR)
+    writeToFile(
+      path.join(EPISODE_MD_DIR, file),
+      contents,
+      path.join('/assets/', path.parse(new URL(podcastUrl).pathname).base)
+    )
+  } catch (e) {
+    console.error(e)
+  }
+}
+
 async function init () {
   const episodes = await fs.readdir(EPISODE_MD_DIR)
   const contents = await Promise.all(
     episodes
       .map(readEpisodeFile)
   )
-
-  contents.map(f => {
-    f.contents += ''
-    return f
-  })
-    .filter(({ file, contents }) => {
-      let podcastFile
-      try {
-        podcastFile = contents.match(/podcastFile: (.+)/)[1]
-      } catch (e) {
-        return true
-      }
-      return !podcastFile || !podcastFile.length
-    })
-    .map(({ file, contents }) => {
-      const podcastUrl = contents.match(/podcastUrl: '(.+)'/)[1]
-      try {
-        downloadFile(podcastUrl, ASSET_FILE_DIR)
-        writeToFile(
-          path.join(EPISODE_MD_DIR, file),
-          contents,
-          path.join('/assets/', path.parse(new URL(podcastUrl).pathname).base)
-        )
-      } catch (e) {
-        console.error(e)
-      }
-    })
+  contents.map(stringifyContent)
+    .filter(noPodcastFile)
+    .map(downloadAndSave)
 }
 
 function downloadFile (url, dest) {
@@ -59,6 +64,7 @@ function writeToFile (file, originalContents, downloadedPath) {
 
 try {
   init()
+    .catch(console.error.bind(console))
 } catch (e) {
   console.log(e)
 }
