096bc950c4469fa15ebb6e5429f144b82956d226
Artem Tsatsin <nuxdie@gmail.com>
2019-01-02 01:36:12 +0100

    making CC happy (#157)

--- lighthouse.test.js
+++ lighthouse.test.js
@@ -1,6 +1,6 @@
-const lighthouse = require('lighthouse') // Node CLI for Lighthouse https://www.npmjs.com/package/lighthouse#using-the-node-cli
+const lighthouse = require('lighthouse')
 const chromeLauncher = require('chrome-launcher') // Launch Chrome from node
-
+/* global jest, test, expect */
 jest.setTimeout(60000)
 
 let SITE_URL
@@ -10,7 +10,8 @@ if (PRODUCTION_BUILD) {
   SITE_URL = `https://frontendweekend.ml`
 } else {
   const PR_NUMBER = (process.env['CIRCLE_PULL_REQUEST']).match(/(\d+)$/)[1]
-  SITE_URL = `https://deploy-preview-${ PR_NUMBER }--frontendweekend.netlify.com`
+  SITE_URL = `https://deploy-preview-${ PR_NUMBER }` +
+    `--frontendweekend.netlify.com`
 }
 
 const launchChromeAndRunLighthouse = (
@@ -28,10 +29,15 @@ const launchChromeAndRunLighthouse = (
 test('All categories score', () =>
   launchChromeAndRunLighthouse(SITE_URL).then(
     ({ lhr: { categories } }) => {
-      expect(categories['accessibility'].score).toBeGreaterThanOrEqual(0.9) // TODO change back to 1 once the winter is over
-      expect(categories['best-practices'].score).toBeGreaterThanOrEqual(0.9) // TODO fix best-practices lh score
-      expect(categories['performance'].score).toBeGreaterThanOrEqual(0.8) // TODO change back to 1 once the winter is over
+      // TODO change back to 1 once the winter is over
+      expect(categories['accessibility'].score).toBeGreaterThanOrEqual(0.9)
+      // TODO fix best-practices lh score
+      expect(categories['best-practices'].score).toBeGreaterThanOrEqual(0.9)
+      // TODO change back to 1 once the winter is over
+      expect(categories['performance'].score).toBeGreaterThanOrEqual(0.8)
       expect(categories['pwa'].score).toBeGreaterThanOrEqual(1)
-      expect(categories['seo'].score).toBeGreaterThanOrEqual(PRODUCTION_BUILD ? 1 : 0.8)
+      expect(categories['seo'].score).toBeGreaterThanOrEqual(
+        PRODUCTION_BUILD ? 1 : 0.8
+      )
     }
   ))
