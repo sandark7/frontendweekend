89a3b380ae43bd4436176b6b7e0ae37f6de02e7f
Artem Tsatsin <nuxdie@gmail.com>
2019-01-16 23:04:31 +0100

    refactor about page close #176

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,44 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import AboutCSSModule from './about.module.css'
 import { I18n } from 'react-i18next'
 import { withI18next } from 'gatsby-plugin-i18next'
 
-class AboutPage extends Component {
-  render () {
-    return (
-      <I18n>
-        { t => (
-          <Layout>
-            <div className={
-              [
-                AboutCSSModule.content_wrapper,
-                'test--content_wrapper'
-              ].join(' ')
-            }>
-              <h1>{t('site_title')}</h1>
-              <p className={ [
-                AboutCSSModule.podcast_description,
-                'test--podcast_description'
-              ].join(' ') }>
-                {t('site_description')}
-              </p>
-              <h2>{t('podcast_host_title')}</h2>
-              <img className={ 'test--host_photo' }
-                src={
-                  'https://avatars0.githubusercontent.com/u/13529513?s=460&v=4'
-                }
-                alt="sandark7"/>
-              <h3>{t('podcast_host_full_name')}</h3>
-              <p>{t('podcast_host_job_title')}</p>
-            </div>
-          </Layout>
-        ) }
-      </I18n>
-    )
-  }
-}
+const AboutPage = () => <I18n>
+  {t => <Layout>
+    <div className={[ AboutCSSModule.content_wrapper,
+      'test--content_wrapper' ].join(' ')}>
+      <h1>{t('site_title')}</h1>
+      <p className={[ AboutCSSModule.podcast_description,
+        'test--podcast_description' ].join(' ')}>
+        {t('site_description')}
+      </p>
+      <h2>{t('podcast_host_title')}</h2>
+      <img className={'test--host_photo'}
+        src={'https://avatars0.githubusercontent.com/u/13529513?s=460&v=4'}
+        alt="sandark7" />
+      <h3>{t('podcast_host_full_name')}</h3>
+      <p>{t('podcast_host_job_title')}</p>
+    </div>
+  </Layout>}
+</I18n>
 
 export default withI18next()(AboutPage)
 
