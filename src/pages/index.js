4b97aad234a9c9ff7b3e991c32c8a012fa116ce0
Artem Tsatsin <nuxdie@gmail.com>
2019-01-16 21:28:56 +0100

    refactor pages/index close #171

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,29 @@ import RandomEpisodes from '../components/randomEpisodes'
 import SubscribeBtn from '../components/subscribeBtn'
 import Support from '../components/support'
 import uniqueRandomArray from 'unique-random-array'
+import { func, string, array, any } from 'prop-types'
 
-class IndexPage extends Component {
+function IndexPageTitle ({ t }) {
+  return (
+    <div className={[
+      IndexCSSModule.title_wrapper,
+      'test--title_wrapper'
+    ].join(' ')}>
+      <h3>{t('latest_episode_title')}</h3>
+      <SubscribeBtn t={t} />
+    </div>
+  )
+}
+
+IndexPageTitle.propTypes = {
+  t: func
+}
+
+class EpisodeWrapper extends Component {
+  static propTypes = {
+    t: func, siteUrl: string, lng: string, comments: array, episode: any
+  }
+
   constructor () {
     super()
     this.state = {
@@ -27,100 +48,105 @@ class IndexPage extends Component {
   }
 
   render () {
-    let {
-      lng,
-      data: {
-        site: {
-          siteMetadata: { siteUrl },
-        },
-        latestEpisode: {
-          edges: [
-            { node: episode }
-          ]
-        },
-        allEpisodes: {
-          edges: allEpisodes
-        },
-        allCommentYaml: { edges: comments },
-      }
-    } = this.props
-    comments = comments.filter(
-      ({ node: comment }) => (comment.slug === episode.frontmatter.name)
-    )
-    allEpisodes.pop()
-    const getRandomEpisode = uniqueRandomArray(allEpisodes)
-    const randomEpisodes = [
-      getRandomEpisode(),
-      getRandomEpisode()
-    ]
+    const { t, episode, siteUrl, comments, lng } = this.props
     return (
-      <I18n>
-        {t => (
-          <Layout title={t('site_title')}>
-            <div className={[
-              IndexCSSModule.main_wrapper,
-              'test--main_wrapper'
-            ].join(' ')}>
-              <div className={[
-                IndexCSSModule.title_wrapper,
-                'test--title_wrapper'
-              ].join(' ')}>
-                <h3>{t('latest_episode_title')}</h3>
-                <SubscribeBtn t={t} />
-              </div>
-              <div>
-                <div className={[
-                  IndexCSSModule.combine_wrapper,
-                  !this.state.shown ? IndexCSSModule.collapsed : '',
-                ].join(' ')}>
-                  <Episode
-                    episode={episode}
-                    siteUrl={siteUrl}
-                    t={t}
-                    comments={comments}
-                    lng={'/' + lng}
-                    onTimecodeClick={() => this.expandDescription()}
-                    onDescriptionClick={() => this.expandDescription()}
-                  ></Episode>
-                </div>
-                <div
-                  onClick={() => this.onShowMoreClick()}
-                  className={[
-                    IndexCSSModule.show_more_wrapper,
-                    'test--show_more'
-                  ].join(' ')}
-                >
-                  <span
-                    className={IndexCSSModule.show_more}
-                  >{
-                      this.state.shown
-                        ? t('show_less_btn_cta')
-                        : t('show_more_btn_cta')
-                    }
-                  </span>
-                </div>
-              </div>
-            </div>
-            <RandomEpisodes
-              t={t}
-              randomEpisodes={randomEpisodes}
-            />
-            <Link className={[
-              IndexCSSModule.archive_link,
-              'test--header_nav-archive',
-            ].join(' ')} to={`/archive/`}>
-              <span>{t('archive_link_text')}</span>
-            </Link>
-            <Support
-              t={t}
-              siteUrl={siteUrl}
-              lng={lng}
-            />
-          </Layout>
-        )}
-      </I18n>
+      <div>
+        <div className={[
+          IndexCSSModule.combine_wrapper,
+          !this.state.shown ? IndexCSSModule.collapsed : '',
+        ].join(' ')}>
+          <Episode
+            episode={episode}
+            siteUrl={siteUrl}
+            t={t}
+            comments={comments}
+            lng={'/' + lng}
+            onTimecodeClick={() => this.expandDescription()}
+            onDescriptionClick={() => this.expandDescription()}
+          ></Episode>
+        </div>
+        <div
+          onClick={() => this.onShowMoreClick()}
+          className={[
+            IndexCSSModule.show_more_wrapper,
+            'test--show_more'
+          ].join(' ')}
+        >
+          <span
+            className={IndexCSSModule.show_more}
+          >{
+              this.state.shown
+                ? t('show_less_btn_cta')
+                : t('show_more_btn_cta')
+            }
+          </span>
+        </div>
+      </div>
     )
   }
+}
+function IndexPageArchiveLink ({ t }) {
+  return (
+    <Link className={[
+      IndexCSSModule.archive_link,
+      'test--header_nav-archive',
+    ].join(' ')} to={`/archive/`}>
+      <span>{t('archive_link_text')}</span>
+    </Link>
+  )
+}
+
+IndexPageArchiveLink.propTypes = {
+  t: func
+}
+function getRandomEpisodes(allEpisodes) {
+  allEpisodes.pop()
+  const getRandomEpisode = uniqueRandomArray(allEpisodes)
+  return [
+    getRandomEpisode(),
+    getRandomEpisode()
+  ]
+}
+function IndexPage ({
+  lng,
+  data: {
+    site: {
+      siteMetadata: { siteUrl },
+    },
+    latestEpisode: {
+      edges: [
+        { node: episode }
+      ]
+    },
+    allEpisodes: {
+      edges: allEpisodes
+    },
+    allCommentYaml: { edges: comments },
+  }
+}) {
+  comments = comments.filter(
+    ({ node: comment }) => (comment.slug === episode.frontmatter.name)
+  )
+  const randomEpisodes = getRandomEpisodes(allEpisodes)
+  return (
+    <I18n>
+      {t => (
+        <Layout title={t('site_title')}>
+          <div className={[
+            IndexCSSModule.main_wrapper,
+            'test--main_wrapper'
+          ].join(' ')}>
+            <IndexPageTitle t={ t }/>
+            <EpisodeWrapper t={t} episode={episode} siteUrl={siteUrl}
+              comments={comments} lng={lng} />
+          </div>
+          <RandomEpisodes t={t} randomEpisodes={randomEpisodes} />
+          <IndexPageArchiveLink t={ t }/>
+          <Support t={t} siteUrl={siteUrl} lng={lng} />
+        </Layout>
+      )}
+    </I18n>
+  )
 }
 
 export default withI18next()(IndexPage)
