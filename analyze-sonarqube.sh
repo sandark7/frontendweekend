7708ec2099ab08775136c50a0cbb347b63011cff
Artem Tsatsin <nuxdie@gmail.com>
2019-01-18 21:43:04 +0100

    fix sonar master reporting (#204)
    * fix sonar master reporting
    
    * mv circleci config
    
    * Revert "fix shellcheck (#202)"
    
    This reverts commit d11337563b532fed922963e2083f17ff78666810.
    
    * add circleci temp_config
    
    * make CC happy again
    
    * rename config for circleci from temp to local
    
    to obtain a new config use something like:
    circleci config process .circleci/config.yml >> .circleci/temp_config.yml

--- analyze-sonarqube.sh
+++ analyze-sonarqube.sh
@@ -3,10 +3,10 @@
 SONAR_VERSION="sonar-scanner-cli-3.2.0.1227-linux"
 SONAR_DIR="sonar-scanner-3.2.0.1227-linux"
 
-wget -P "$HOME" -N "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/${SONAR_VERSION}.zip"
-unzip -d "$HOME" "$HOME"/"$SONAR_VERSION".zip
+wget -P "$HOME" -N "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/$SONAR_VERSION.zip"
+unzip -d "$HOME" "$HOME/$SONAR_VERSION.zip"
 
-echo "${CIRCLE_PULL_REQUEST}"
+echo "$CIRCLE_PULL_REQUEST"
 
 PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
 
@@ -16,9 +16,10 @@ DEFAULT_SONAR_PARAMS="-Dsonar.login=$SONARQUBE_LOGIN \
                       -Dsonar.sources=./src \
                       -Dsonar.host.url=https://sonarcloud.io"
 
-if [[ -n "$CIRCLE_PULL_REQUEST" ]]; then
+if [[ -n $CIRCLE_PULL_REQUEST ]]; then
   echo "Preview analyzing ${CIRCLE_PULL_REQUEST} by SonarQube Github Plugin"
-  "$HOME"/"$SONAR_DIR"/bin/sonar-scanner "$DEFAULT_SONAR_PARAMS" \
+  # shellcheck disable=SC2086
+  "$HOME/$SONAR_DIR"/bin/sonar-scanner $DEFAULT_SONAR_PARAMS \
     -Dsonar.pullrequest.base=master \
     -Dsonar.pullrequest.branch="$CIRCLE_BRANCH" \
     -Dsonar.pullrequest.key="$PR_NUMBER" \
@@ -28,5 +29,6 @@ fi
 
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
     echo "Analyzing ${CIRCLE_BRANCH} branch to push issues to SonarQube server"
-    "$HOME"/"$SONAR_DIR"/bin/sonar-scanner "$DEFAULT_SONAR_PARAMS";
+    # shellcheck disable=SC2086
+    "$HOME/$SONAR_DIR"/bin/sonar-scanner $DEFAULT_SONAR_PARAMS;
 fi
