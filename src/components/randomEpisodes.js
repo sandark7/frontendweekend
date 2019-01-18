ee7a7cf26ea83ea3c08ff9c26f8ec58ffae419c9
Artem Tsatsin <nuxdie@gmail.com>
2019-01-18 16:21:42 +0100

    refactor randomEpisodes (#200)
    close #177

--- src/components/randomEpisodes.js
+++ src/components/randomEpisodes.js
@@ -1,36 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import RandomEpisodesCSSModule from './randomEpisodes.module.css'
 import EpisodeItem from './episodeItem'
+import { func, array } from 'prop-types'
 
-class RandomEpisodes extends Component {
-  render () {
-    const {
-      randomEpisodes,
-      t,
-    } = this.props
-    return (
-      <div
-        className={[
-          RandomEpisodesCSSModule.main_wrapper,
-          'test--random_episode_main_wrapper',
-        ].join(' ')}
-      >
-        <h3
-          className={[
-            RandomEpisodesCSSModule.random_episode_wrapper,
-            'test--random_episode_wrapper',
-          ].join(' ')}
-        >{t('random_episode_title')}</h3>
-        {randomEpisodes.map(({ node: episode }) => (
-          <EpisodeItem
-            key={episode.id}
-            episode={episode}
-            t={t}
-          />
-        ))}
-      </div>
-    )
-  }
-}
+const RandomEpisodes = ({ randomEpisodes, t }) => <div className={[
+  RandomEpisodesCSSModule.main_wrapper, 'test--random_episode_main_wrapper'
+].join(' ')}>
+  <h3 className={[
+    RandomEpisodesCSSModule.random_episode_wrapper,
+    'test--random_episode_wrapper'
+  ].join(' ')}>{t('random_episode_title')}</h3>
+  {randomEpisodes.map(({
+    node: episode
+  }) => <EpisodeItem key={episode.id} episode={episode} t={t} />)}
+</div>
+
+RandomEpisodes.propTypes = { t: func, randomEpisodes: array }
 
 export default RandomEpisodes
