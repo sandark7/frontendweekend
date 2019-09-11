bce2c614b2b994bbe3b134085f1021928427b198
Andrew Smirnov <sandark7@users.noreply.github.com>
2019-09-12 01:52:16 +0300

    Update commit-to-master.sh

--- commit-to-master.sh
+++ commit-to-master.sh
@@ -5,4 +5,4 @@ git config user.email "sc-import-bot@frontendweekend.ml"
 git config user.name "SC Import Bot"
 git add .
 git commit -am "new episodes & comments commit"
-git push -q https://${SC_IMPORT_GITHUB_TOKEN}@github.com/nuxdie/frontendweekend.git master
+git push -q https://${SC_IMPORT_GITHUB_TOKEN}@github.com/sandark7/frontendweekend.git master
